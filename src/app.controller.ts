75486a539681b0333f0eeb69908e547bc5f9e5b7
ph3 <100564304+ph3nac@users.noreply.github.com>
2024-07-27 23:58:09 +0900

    add: prisma

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+
+import { UserService } from './user.service';
 import { AppService } from './app.service';
+import { User as UserModel } from '@prisma/client';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly userService: UserService,
+  ) {}
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('user')
+  async signupUser(
+    @Body() userData: { name?: string; email: string },
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+
+  @Get('user/:id')
+  async getUser(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.user({ id: Number(id) });
+  }
+
+  @Post('login')
+  async login(
+    @Body() credentials: { email: string; password: string },
+  ): Promise<UserModel> {
+    return this.userService.login(credentials);
+  }
+
+  @Post('logout')
+  async logout(@Body() token: string): Promise<void> {
+    return null;
   }
 }
